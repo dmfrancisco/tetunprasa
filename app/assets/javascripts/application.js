2267a1c1a02483fa79cac54ca42585f2a3f50697
David Francisco <dmfrancisc@gmail.com>
2017-04-01 21:42:48 +0100

    Search with turbolinks and minor changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,72 +2,80 @@
 //= require jquery_ujs
 //= require turbolinks
 
-var Sheet;
-var Marker;
-
 $("html").addClass("js");
 
-$(document).ready(function () {
-  Sheet = {
-    show: function () {
-      var $sheet = $("#sheet");
+var Sheet = {
+  show: function () {
+    var $sheet = $("#sheet");
 
-      if ($sheet.is(":visible")) return;
-      $sheet.addClass("slideInRight").show();
-      setTimeout(function () { $sheet.removeClass("slideInRight"); }, 750);
-    },
-    hide: function () {
-      var $sheet = $("#sheet");
+    if ($sheet.is(":visible")) return;
+    $sheet.addClass("slideInRight").show();
+    setTimeout(function () { $sheet.removeClass("slideInRight"); }, 750);
+  },
+  hide: function () {
+    var $sheet = $("#sheet");
 
-      if (!$sheet.is(":visible")) return;
-      $sheet.addClass("slideOutRight");
-      setTimeout(function () { $sheet.removeClass("slideOutRight").hide(); }, 750);
+    if (!$sheet.is(":visible")) return;
+    $sheet.addClass("slideOutRight");
+    setTimeout(function () { $sheet.removeClass("slideOutRight").hide(); }, 750);
+  }
+}
+
+var Marker = {
+  mark: function () {
+    var keyword = $("input[name='buka']").val().replace(/\"/g, '');
+    var $entries = $("#entries");
+
+    if ($entries.find(".Entry--empty").length == 0) {
+      $entries.unmark({
+        done: function() {
+          $("#entries").mark(keyword);
+        }
+      });
     }
   }
+}
 
-  // Hide side sheet on click
-  $(document).on("click", "#sheet-close", function (e) {
-    e.preventDefault();
-    Sheet.hide();
-  });
+// Hide side sheet on click
+$(document).on("click", "#sheet-close", function (e) {
+  e.preventDefault();
+  Sheet.hide();
+});
 
-  // Hide an element on click
-  //
-  // Elements with the `data-close` attribute can close a given element.
-  // The element to be closed is specified by the `data-close` attribute.
-  // If no value is specified, the parent will be closed.
-  // An optional `data-animation` class can be passed.
-  // If no value is specified, the element will just be hidden.
-  $(document).on("click", "[data-close]", function (e) {
-    e.preventDefault();
+// Hide an element on click
+//
+// Elements with the `data-close` attribute can close a given element.
+// The element to be closed is specified by the `data-close` attribute.
+// If no value is specified, the parent will be closed.
+// An optional `data-animation` class can be passed.
+// If no value is specified, the element will just be hidden.
+$(document).on("click", "[data-close]", function (e) {
+  e.preventDefault();
 
-    var $this = $(this);
-    var $element = $($this.data("close") || $this.parent());
-    var animationClass = $this.data("animation");
+  var $this = $(this);
+  var $element = $($this.data("close") || $this.parent());
+  var animationClass = $this.data("animation");
 
-    if (animationClass) {
-      $element.addClass(animationClass);
-      setTimeout(function () { $element.removeClass(animationClass).hide(); }, 1050);
-    } else {
-      $element.hide();
-    }
-  });
+  if (animationClass) {
+    $element.addClass(animationClass);
+    setTimeout(function () { $element.removeClass(animationClass).hide(); }, 1050);
+  } else {
+    $element.hide();
+  }
+});
 
-  // Remove the initial snackbar animation so it doesn't happen again
-  setTimeout(function() {
-    $(".Snackbar-message").removeClass("bounceInUp");
-  }, 1050);
+// Setup infinite scrolling
+$(window).on("scroll", function () {
+  var url = $('.pagination .next a').attr('href');
 
-  // Setup infinite scrolling
-  $(window).on("scroll", function () {
-    var url = $('.pagination .next a').attr('href');
+  if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 200) {
+    $('.pagination').text('');
+    $.getScript(url);
+  }
+});
 
-    if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 200) {
-      $('.pagination').text('');
-      $.getScript(url);
-    }
-  });
-
+// Application code to be called everytime the body changes
+$(document).on('turbolinks:load', function () {
   // Modern browsers seem to keep the input value when the user hits back
   // This makes it seem the input value and search results don't match
   // It may also result in content being highlighted because of the `mark()` below
@@ -77,19 +85,5 @@ $(document).ready(function () {
   }
 
   // Highlight matches
-  Marker = {
-    mark: function () {
-      var keyword = $("input[name='buka']").val().replace(/\"/g, '');
-      var $entries = $("#entries");
-
-      if ($entries.find(".Entry--empty").length == 0) {
-        $entries.unmark({
-          done: function() {
-            $("#entries").mark(keyword);
-          }
-        });
-      }
-    }
-  }
   Marker.mark();
 });
